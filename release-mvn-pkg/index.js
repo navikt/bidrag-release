78534811f7aca4b33199e35f5cb5eec5cf1c4287
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-11-29 16:05:03 +0100

    remove input to release action

--- release-mvn-pkg/index.js
+++ release-mvn-pkg/index.js
@@ -1,26 +1,13 @@
 const core = require("@actions/core");
 const exec = require("@actions/exec");
-const fs = require("fs");
-const process = require("process");
 
 async function run() {
   try {
     core.debug(`filepath: ${__dirname}`);
 
-    let newSnapshotVersion = core.getInput(
-        "new-snapshot-version", {required: true}
-    );
-
-    let newSnapshotFile = `${process.env.GITHUB_WORKSPACE}/.new-snapshot-version`;
-
-    try {
-      fs.writeFileSync(newSnapshotFile, newSnapshotVersion);
-    } catch (err) {
-      core.setFailed(err.message)
-    }
-
     // Execute release bash script
     await exec.exec(`${__dirname}/src/release.sh`);
+
   } catch (error) {
     core.setFailed(error.message);
   }
