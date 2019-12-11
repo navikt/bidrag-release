2293510c3d2615c9d48ad10ae12b0b29052e1d5d
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2019-12-11 11:33:39 +0100

    the name of the files as input

--- release-prepare-mvn-pkg/prepare-release.sh
+++ release-prepare-mvn-pkg/prepare-release.sh
@@ -7,15 +7,15 @@ set -e
 RELEASE_VERSION=$(cat pom.xml | grep version | grep SNAPSHOT | \
   sed 's/version//g' | sed 's/  //' | sed 's/-SNAPSHOT//' | sed 's;[</>];;g')
 
-# - writes release version (1.2.3) to file for RELEASE_VERSION_FILE
-echo "$RELEASE_VERSION" > "$RELEASE_VERSION_FILE"
+# - writes release version (1.2.3) to file for INPUT_RELEASE_VERSION_FILE_NAME
+echo "$RELEASE_VERSION" > "$INPUT_RELEASE_VERSION_FILE_NAME"
 
 # updates to version 1.2.4-SNAPSHOT
 mvn -B release:update-versions
 
-# writes new snapshot version (1.2.4-SNAPSHOT) to file for NEW_SNAPSHOT_VERSION_FILE
+# writes new snapshot version (1.2.4-SNNAPSHOT) to file INPUT_NEW_SNAPSHOT_VERSION_FILE_NAME
 cat pom.xml | grep version | grep SNAPSHOT | \
-  sed 's/version//g' | sed 's/  //' | sed 's;[</>];;g' > "$NEW_SNAPSHOT_VERSION_FILE"
+  sed 's/version//g' | sed 's/  //' | sed 's;[</>];;g' > "$INPUT_NEW_SNAPSHOT_VERSION_FILE_NAME"
 
 # fetch git commit sha to COMMIT_SHA, ex: 22ea0ff
 COMMIT_SHA=$(git rev-parse --short=12 HEAD)
