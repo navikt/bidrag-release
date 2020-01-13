f81f4451e71473033d43832b52e13749348ac2ae
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-01-13 15:11:52 +0000

    Fresh build of actions using npm and ncc

--- release-verify-auto-deploy/dist/verify-deploy.sh
+++ release-verify-auto-deploy/dist/verify-deploy.sh
@@ -13,9 +13,9 @@ if [ ! -f "$INPUT_CHANGELOG_FILE" ]
     exit 1;
 fi
 
-RELEASE_VERSION="$(cat "$INPUT_RELEASE_VERSION_FILE")"                # the release version to deploy
-RELEASE_TABLE="$(cat "$INPUT_CHANGELOG_FILE" | grep '|' )"            # the release table in the changelog file
-COUNT="$(echo "$RELEASE_TABLE" | grep -c -q "$RELEASE_VERSION")"      # count all mentions of 'RELEASE_VERSION' in the 'RELEASE_TABLE' from the changelog
+RELEASE_VERSION="$(cat "$INPUT_RELEASE_VERSION_FILE")"                  # the release version to deploy
+RELEASE_TABLE="$(cat "$INPUT_CHANGELOG_FILE" | grep '|' )"              # the release table in the changelog file
+COUNT="$(echo "$RELEASE_TABLE" | grep -c --silent "$RELEASE_VERSION")"  # count all mentions of 'RELEASE_VERSION' in the 'RELEASE_TABLE' from the changelog
 
 echo "echo Found $COUNT mentioning(s) of $RELEASE_VERSION in $INPUT_CHANGELOG_FILE."
 
