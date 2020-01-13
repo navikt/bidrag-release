07b7939f20e808e1a9b1ae8b0d762982ff687d43
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-01-13 15:38:58 +0100

    quiet count to allow fail fast

--- release-verify-auto-deploy/verify-deploy.sh
+++ release-verify-auto-deploy/verify-deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 if [ ! -f "$INPUT_RELEASE_VERSION_FILE" ]
   then
@@ -14,7 +15,7 @@ fi
 
 RELEASE_VERSION="$(cat "$INPUT_RELEASE_VERSION_FILE")"                # the release version to deploy
 RELEASE_TABLE="$(cat "$INPUT_CHANGELOG_FILE" | grep '|' )"            # the release table in the changelog file
-COUNT="$(echo "$RELEASE_TABLE" | grep -c "$RELEASE_VERSION")"         # count all mentions of 'RELEASE_VERSION' in the 'RELEASE_TABLE' from the changelog
+COUNT="$(echo "$RELEASE_TABLE" | grep -c -q "$RELEASE_VERSION")"      # count all mentions of 'RELEASE_VERSION' in the 'RELEASE_TABLE' from the changelog
 
 echo "echo Found $COUNT mentioning(s) of $RELEASE_VERSION in $INPUT_CHANGELOG_FILE."
 
