bbc38ddda34a5d4340ed219a6f7d745e3b005bf7
Tor Egil Jacobsen <tor.egil.jacobsen@nav.no>
2020-02-07 11:59:17 +0000

    2020-02-07:11:59:17: Fresh build of actions with npm and ncc

--- release-mvn-pkg/dist/release.sh
+++ release-mvn-pkg/dist/release.sh
@@ -29,7 +29,7 @@ if [ ! -f "$INPUT_NEW_SNAPSHOT_VERSION_FILE" ]
 fi
 
 echo "Running release"
-mvn -B --settings maven-settings.xml deploy -DskipTests -Dmaven.wagon.http.pool=false
+mvn -B --settings maven-settings.xml deploy -e -DskipTests -Dmaven.wagon.http.pool=false
 
 NEW_SNAPSHOT_VERSION=$(cat $INPUT_NEW_SNAPSHOT_VERSION_FILE)
 
